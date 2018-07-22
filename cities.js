42cb008a4f03673675bc432ef09136d32e654a48
roadwarrior99 <colinmhayes@gmail.com>
2018-07-21 20:35:56 -0500

    Add files via upload
    The clocks are configured from the JSON object in cities.js

---
+++ cities.js
@@ -0,0 +1,54 @@
+var cities = [{
+	"Name":"Chicago",
+	"TimeZone": "America/Chicago",
+	"Importance":1,
+  "time":""
+},
+{
+	"Name":"Denver",
+	"TimeZone":"America/Denver",
+	"Importance":2,
+  "time":""
+},
+{
+	"Name":"Sacremento",
+	"TimeZone": "America/Los_Angeles",
+	"Importance": 2,
+  "time":""
+},
+{
+	"Name":"St. Petersburg",
+	"TimeZone":"America/New_York",
+	"Importance":2,
+  "time":""
+},
+{
+	"Name":"Dublin",
+	"TimeZone":"Europe/Dublin",
+	"Importnace":3,
+  "time":""
+},
+{
+	"Name":"Brussels",
+	"TimeZone":"Europe/Brussels",
+	"Importance":3,
+  "time":""
+},
+{
+	"Name":"Pune",
+	"TimeZone":"Asia/Colombo",
+	"Importance":1,
+  "time":""
+},
+{
+	"Name":"Perth",
+	"TimeZone":"Australia/Perth",
+	"Importance":3,
+  "time":""
+},
+{
+	"Name":"Sydney",
+	"TimeZone":"Australia/Sydney",
+	"Importance":3,
+  "time":""
+}]
